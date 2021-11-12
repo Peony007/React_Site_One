7dc12b52b100f120cc44314fd137fe56d244d5b3
WebCaesar <super_caesar@outlook.com>
2021-11-12 10:46:31 +0800

    FixStoreJs

--- src/store.js
+++ src/store.js
@@ -1,18 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 // import { createStoreHook } from "react-redux";
 
+import { composeWithDevTools } from 'redux-devtools-extension';
 const initialState = {};
-
 const middleware = [thunk];
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const applied = composeWithDevTools(applyMiddleware(...middleware)); //error!
+const store = createStore(rootReducer, initialState, applied);
 
 export default store;
